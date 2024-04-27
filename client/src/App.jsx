918b23ab72c8f6624fe787e8cf0ae904adb40fa5
LiktoChan123456 <115712830+LiktoChan123456@users.noreply.github.com>
2024-04-27 18:17:15 +0800

    install react js and tailwind css and create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+      <h2 className="text-3xl text-red-500">Hello world</h2>
+      <p className='p-3'>life saving thearpies apply, to be followewd by seconds doctor opinions regarding necessity, urgency and best interest principle</p>
+    </div>
+  )
+}
