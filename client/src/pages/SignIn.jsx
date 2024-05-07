484018c99fe6d05da8c0ccf0ebd93fde35eb00df
LiktoChan123456 <115712830+LiktoChan123456@users.noreply.github.com>
2024-05-07 19:26:15 +0800

    complete signin page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,100 @@
-import React from 'react'
+import { Alert, Button, Label, Spinner, TextInput } from 'flowbite-react'
+import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [errorMessages, setErrorMessages] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handleChange = (e) => {
+    setFormData({...formData, [e.target.id]: e.target.value.trim()})
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password){
+      return setErrorMessages("Please fill out the form")
+    }
+    try{
+      setLoading(true);
+      setErrorMessages(null)
+      const res = await fetch('/api/auth/signin', {
+        method: "POST",
+        headers: { 'Content-type': "application/json" },
+        body: JSON.stringify(formData)
+      });
+      const data = await res.json();
+      if (data.success ===false){
+        return setErrorMessages(data.message)
+      }
+      setLoading(false)
+      if (res.ok ===true){
+        navigate('/')
+      }
+    } catch (error){
+      setErrorMessages(error.message)
+      setLoading(false)
+    }
+  }
+
   return (
-    <div>
-      SignIn
+    <div className='min-h-screen mt-20'>
+      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+        {/* left side */}
+        <div className='flex-1'>
+        <Link to="/" className="font-bold dark:text-white text-4xl">
+            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">Techtipsgo</span>
+            Blog
+        </Link>
+        <p className='text-sm mt-5'>This is techtipsgo blog. You can sign in with your email and password or with google</p>
+        </div>
+        {/* right side */}
+        <div className='flex-1'>
+          <form className='flex flex-col gap-4' onSubmit={handleSubmit}>
+            <div className=''>
+              <Label value="Your email"></Label>
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email" 
+                onChange={handleChange}
+              ></TextInput>
+            </div>
+            <div className=''>
+              <Label value="Your Password"></Label>
+              <TextInput
+                type="password"
+                placeholder="***********"
+                id="password" 
+                onChange={handleChange}
+              ></TextInput>
+            </div>
+            <Button gradientDuoTone="purpleToPink" type="submit" disabled={loading}>
+              {
+                loading ? (
+                  <Spinner size='sm'>
+                    <span className='pl-3'>Loading...</span>
+                  </Spinner>
+                  
+                ) : 'Sign In'
+              }
+            </Button>
+          </form>
+          <div className="flex gap-2 text-sm mt-5">
+            <span>Don't have an account?</span>
+            <Link to="/sign-up" className="text-blue-500">Sign Up</Link>
+          </div>
+          {
+            errorMessages && (
+              <Alert className='mt-5' color='failure'>
+                {errorMessages}
+              </Alert>
+            )
+          }
+        </div>
+      </div>
+    
     </div>
+  
   )
 }
