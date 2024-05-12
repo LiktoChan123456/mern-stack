394b9057c44aa5da8c5b6ea6f711a90e5dab97e7
LiktoChan123456 <115712830+LiktoChan123456@users.noreply.github.com>
2024-05-12 20:50:04 +0800

    complete user image upload functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,16 +1,95 @@
-import { Button, TextInput } from 'flowbite-react'
-import React from 'react'
-import {useSelector} from 'react-redux'
-
+import { Alert, Button, TextInput } from 'flowbite-react';
+import React, { useEffect, useRef, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { getDownloadURL, getStorage, uploadBytes, ref } from 'firebase/storage'; // Amended: Added ref
+import { app } from '../firebase';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 export default function DashProfile() {
-  const {currentUser} = useSelector((state) => state.user)
+  const { currentUser } = useSelector((state) => state.user);
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(0); 
+  const [imageFile, setImageFile] = useState(null);
+  const [imageFileUrl, setImageFileUrl] = useState(null);
+  const [imageFileError, setImageFileError] = useState(null);
+  const filePickerRef = useRef();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file){
+      setImageFile(file);
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  };
+
+  useEffect(() => {
+    if (imageFile){
+      uploadImage();
+    }
+  }, [imageFile]);
+
+  const uploadImage = async () => {
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage, fileName);
+    const uploadTask = uploadBytes(storageRef, imageFile);
+
+    uploadTask.then(
+      "state_changed", 
+      (snapshot) => { 
+      console.log("Upload completed successfully")
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      setImageFileUploadProgress(progress.toFixed(0));
+      
+    },
+    (error) => {
+      setImageFileError("Could not upload image: File must be less than 2MB")
+      setImageFileUploadProgress(null)
+      setImageFile(null)
+      setImageFileUrl(null)
+    },
+    () => {
+    
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) =>{
+        setImageFileUrl(downloadURL);
+      });
+    }
+  );
+  };
+
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="my-7 text-center font-semibold text-3xl">Profile</h1>
       <form className="flex flex-col gap-4">
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
-          <img src={currentUser.profilePicture} alt="user" className='rounded-full w-full h-full object-cover border-8 border-[#b4acac]'></img>
+        <input type="file" accept='image/*' onChange={handleImageChange} ref={filePickerRef} hidden/>
+        <div className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
+          <img src={imageFileUrl || currentUser.profilePicture} alt="user" className='rounded-full w-full h-full object-cover border-8 border-[#b4acac65]' onClick={()=> filePickerRef.current.click()}></img>
+        
+        {imageFileUploadProgress && (
+          <CircularProgressbar 
+          value={imageFileUploadProgress || 0} 
+          text={`${imageFileUploadProgress}%`} strokeWidth={5} 
+          style={{
+            root:{
+              width: '100%',
+              height: '100%',
+              position: 'absolute',
+              top: 0,
+              left:0,
+            }, path:{
+              stroke: `rgba(62, 152, 199 ${imageFileUploadProgress / 100})`,
+            },
+          }}
+          />
+        ) 
+        }
+
+        
+        {imageFileError && 
+          <Alert color='failure'>
+            {imageFileError}
+          </Alert>
+        }
         </div>
         <TextInput type="text" id="username" placeholder="username" defaultValue={currentUser.username}/>
         <TextInput type="email" id="email" placeholder="email" defaultValue={currentUser.email}/>
@@ -23,5 +102,5 @@ export default function DashProfile() {
         <span className="cursor-pointer">Sign out</span>
       </div>
     </div>
-  )
+  );
 }
